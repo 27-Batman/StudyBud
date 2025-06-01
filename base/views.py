865f958c6adbf6f405ea36d9d6d9e916a184a6f1
27-Batman <anuragsing092@gmail.com>
2025-06-01 14:25:05 +0545

    setup templates

--- base/views.py
+++ base/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+rooms = [
+    {'id':1, 'name':"Hello There"},
+    {'id':2, 'name':"How are you"},
+    {'id':3, 'name':"Let code together"}
+]
 
 def home(request):
-    return HttpResponse('Home page')
+    return render(request, 'base/home.html', {'rooms':rooms})
 
-def room(request):
-    return HttpResponse('Room')
+def room(request, pk):
+    room = 0
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+
+    return render(request, 'base/room.html', context)
